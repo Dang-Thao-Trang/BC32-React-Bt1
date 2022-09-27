2637f3496af0f65d8f035df6dce3694513bf4192
DangThaoTrang <thaotrangcongviec@gmail.com>
2022-09-27 18:35:23 +0700

    source

---
+++ src/1_Component/BT1/BT1.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import Navbar from "./Navbar";
+
+const BT1 = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default BT1;
